a92610dcc678ef7f0b46d42d5e2de7550ac14f46
HimashaHerath <himasha626@gmail.com>
2024-04-13 14:17:54 +0530

    adds client side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,51 +1,59 @@
 "use client";
-import { Button, Callout, TextField } from "@radix-ui/themes";
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller, set } from "react-hook-form";
 import React, { useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { CreateIssueSchema } from "@/app/validationSchema";
+import { z } from "zod";
 
-type IssueForm = {
-  title: string;
-  description: string;
-};
+type IssueForm = z.infer<typeof CreateIssueSchema>;
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, handleSubmit, control } = useForm<IssueForm>();
-  const [error,seterror] = useState('');
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<IssueForm>({ resolver: zodResolver(CreateIssueSchema) });
+  const [error, seterror] = useState("");
 
   const onSubmit = async (data: IssueForm) => {
     try {
       await axios.post("/api/v1/issues", data);
-      router.push("/issues"); 
+      router.push("/issues");
     } catch (error) {
-      seterror('Failed to submit issue');
+      seterror("Failed to submit issue");
     }
   };
   return (
     <div className="max-w-xl">
-        {error &&<Callout.Root color="red" className="mb-5">
-            <Callout.Text>{error}</Callout.Text> 
-            </Callout.Root>}
-    <form className="space-y-3" onSubmit={handleSubmit(onSubmit)}>
-      <TextField.Root
-        placeholder="Title "
-        {...register("title")}
-      ></TextField.Root>
-      <Controller
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description" {...field} />
-        )}
-        name="description"
-        control={control}
-      />
-      <Button>Submit New Issue</Button>
-    </form>
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form className="space-y-3" onSubmit={handleSubmit(onSubmit)}>
+        <TextField.Root placeholder="Title " {...register("title")}>
+        </TextField.Root>
+          {errors.title && <Text color="red" as="p">{errors.title.message}</Text>}
+        <Controller
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+          name="description"
+          control={control}
+        />
+        {errors.description && <Text color="red" as="p">{errors.description.message}</Text>}
+        <Button>Submit New Issue</Button>
+      </form>
     </div>
   );
 };
 
 export default NewIssuePage;
+
